8f28da8d3321ba600a2e831d66b621de5b8457f1
kelebetengida <engida398@gmail.com>
2024-12-09 18:11:59 -0800

    String

---
+++ JS/Class Notes/Week 2/datatype.js
@@ -0,0 +1,14 @@
+//Data types 
+
+//We have two different data types. WHat are they?
+    // primitive 
+        // Number
+        // String
+        // Boolean
+        // null
+        // undefined
+        // Symbol
+    //non primitive
+        //objects
+        //arrays
+        //functions
